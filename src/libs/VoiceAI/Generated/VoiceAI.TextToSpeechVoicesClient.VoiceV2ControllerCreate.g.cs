fd1c8c46203d7687c22ab654e1ab2790127edfe2
Konstantin S. <havendv@gmail.com>
2026-05-24 10:32:12 +0400

    feat: Updated OpenAPI spec (#14)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/VoiceAI/Generated/VoiceAI.TextToSpeechVoicesClient.VoiceV2ControllerCreate.g.cs
+++ src/libs/VoiceAI/Generated/VoiceAI.TextToSpeechVoicesClient.VoiceV2ControllerCreate.g.cs
@@ -358,17 +358,16 @@ namespace VoiceAI
                                     __exception_400 = __ex;
                                 }
 
-                                throw new global::VoiceAI.ApiException(
+
+                                throw global::VoiceAI.ApiException.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_400 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_400,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_400,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_400,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
 
                             if (__effectiveReadResponseAsString)
@@ -402,17 +401,15 @@ namespace VoiceAI
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::VoiceAI.ApiException(
+                                    throw global::VoiceAI.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -449,17 +446,15 @@ namespace VoiceAI
                                     {
                                     }
 
-                                    throw new global::VoiceAI.ApiException(
+                                    throw global::VoiceAI.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
